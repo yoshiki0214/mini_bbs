ceb0d35fd36e38af310d56e2715cf850dd6f5f3e
yoshiki0214 <69489465+yoshiki0214@users.noreply.github.com>
2021-01-11 13:40:30 +0900

    Update dbconnect.php

--- mini_bbs/dbconnect.php
+++ mini_bbs/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
   try{
-    $db = new PDO('mysql:dbname=heroku_7a5ea123b249485; host=us-cdbr-east-02.cleardb.com; port=8889; charset=utf8', 'bb0918e066e306', '136f9901');
+    $db = new PDO('mysql:dbname=heroku_7a5ea123b249485; host=us-cdbr-east-02.cleardb.com; port=3306; charset=utf8', 'bb0918e066e306', '136f9901');
   }catch(PDOException $e){
     print('DB接続エラー：' . $e->getMessage());
   }
